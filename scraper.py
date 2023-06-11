171b6be857b1d235f300d88d2ce433686b7f49b8
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2023-06-11 11:39:47 -0400

    brand and model

--- scraper.py
+++ scraper.py
@@ -22,14 +22,20 @@ headers= {
     "method": "GET",
     "mode": "cors"
 }
+#"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0", 
+#"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
 page_to_scrape = requests.get(URL,headers=headers) 
 soup = BeautifulSoup(page_to_scrape.text, 'html.parser') 
-
 products = soup.findAll('li', attrs={"class":"product_wrapper"})
+
 for product in products:
-    print(product[1])
-#with open('output2.txt', 'a') as f:
-#    f.write(str(products)) 
+    print((product.findAll("a")[1].get("data-brand")) + " " + (product.findAll("a")[1].get("data-name").replace('&quot','"'))) #brand and model
+    print(product.find("li", attrs={"class":"spec_1 primary"}).text) #cpu
+    print(product.find("li", attrs={"class":"spec_2 primary"}).text) #ram
+    print(product.find("li", attrs={"class":"spec_3 primary"}).text) #storage
+    print("--------------------------")
+with open('output2.txt', 'a') as f:
+    f.write(str(products)) 
 #prices = soup.findAll('small', attrs={"class":"author"})
 #cpus = soup.findAll('small', attrs={"class":"author"})
 #rams = soup.findAll('small', attrs={"class":"author"})
